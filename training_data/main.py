a001928fd74affaaeaf9a6a4e2311a01ceda1ce9
GucciCologne <augiestlouis@gmail.com>
2022-02-25 15:35:44 -0500

    added capability to find the exact value of each brk value for each row (0 or 1)

--- training_data/main.py
+++ training_data/main.py
@@ -2,27 +2,8 @@ import csv
 import numpy as np
 import pandas as pd
 
-a = open('training_input.csv')
-b = open('training_target.csv')
-
 ap_VA = pd.read_csv("training_input.csv", skiprows=0)
 bp_BA = pd.read_csv("training_target.csv", skiprows=0)
-
-csv_reader_a = csv.reader(a)
-csv_reader_b = csv.reader(b)
-
-training_input = np.array([])
-training_target = np.array([])
-
-
-for line in csv_reader_a:
-    training_input = np.append(training_input, line)
-
-for line in csv_reader_b:
-    training_target = np.append(training_target, line)
-
-a.close()
-b.close()
 
 df = ap_VA
 df.insert(8, "BRKa", bp_BA['BRKa'], True)
@@ -30,6 +11,17 @@ df.insert(9, "BRKb", bp_BA['BRKb'], True)
 df.insert(10, "BRKc", bp_BA['BRKc'], True)
 df.insert(11, "BRKd", bp_BA['BRKd'], True)
 
-print(training_input, training_target)
-print(df)
+vals= df.values
+
+for i in vals:
+    brka = i[8]
+    brkb = i[9]
+    brkc = i[10]
+    brkd = i[11]
+    #print("brka: %f" % (brka))
+    #print("brkb: %f" % (brkb))
+    #print("brkc: %f" % (brkc))
+    #print("brkd: %f" % (brkd))
+
+
 
